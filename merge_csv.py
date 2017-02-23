3064154c365d734cc82a7eaac6d104c1ef72aeb6
GOWTHAM MALLIKARJUNA <gowtham.91m@gmail.com>
2017-02-23 10:45:55 -0600

    Update merge_csv.py

--- merge_csv.py
+++ merge_csv.py
@@ -7,12 +7,11 @@ Created on Tue Dec 20 15:42:20 2016
 import os
 import pandas as pd
 #pd.set_option('display.max_columns', None) #dilplay all the columns
-#pd.set_option('display.max_rows', None) #dilplay all teh columns
-#pd.set_option('display.expand_frame_repr', None) #dilplay all teh columns
-#pd.set_option('display.max_colwidth',-1) # display full text in the column
+#pd.set_option('display.max_rows', None) #dilplay all the columns
+#pd.set_option('display.expand_frame_repr', None)
 def main():
     # get location of script
-    # This works only on command prompt. If working on any IDE, set current woking directory manually
+    # This works only on command prompt. If working on any IDE, set current woking directory
     wd = os.path.dirname(os.path.realpath(__file__))
     # change working directory to script location
     #wd='C:\\Users\\gmallik\\Downloads\\OSUAA\\merge\\merge'
@@ -21,17 +20,17 @@ def main():
     if not os.path.isdir('export'):
         os.mkdir('export')
     # set relative path from working directory
-    export_path = wd +'\\export\\'
-    data_path = wd+'\\data\\'
-    recon_path=wd+'\\reconciliation\\'
+    export_path = os.path.join(wd,'export')
+    data_path = os.path.join(wd,'data')
+    recon_path = os.path.join(wd,'reconciliation_files')
     files=os.listdir(data_path)
     recon=os.listdir(recon_path)
     
-    # aggridage and store all reconciliation files
+    # concatenate reconciliation files to single dataframe
     recon_data = 0
     for k in recon:
         if k[-4:]=='.csv':
-            df= pd.read_csv(recon_path+k,parse_dates=['CnBio_Birth_date'])
+            df= pd.read_csv(recon_path+'\\'+k,parse_dates=['CnBio_Birth_date'])
             df.rename(columns={'First Name':'First_Name','Last Name':'Last_Name'},inplace=True)
             df = df.ix[:,['CnBio_ID','First_Name','Last_Name','CnBio_Birth_date']]
             df['Month']=k[:3]
@@ -39,14 +38,16 @@ def main():
                 recon_data=df
             else:
                 recon_data=recon_data.append(df)
+                
     recon_data = recon_data[(pd.isnull(recon_data['CnBio_ID'])==False)]
     recon_data.drop_duplicates(['First_Name','Last_Name','CnBio_ID','CnBio_Birth_date'],inplace=True)
     recon_data['First_Name']=pd.core.strings.str_strip(recon_data['First_Name'])
     recon_data['Last_Name']=pd.core.strings.str_strip(recon_data['Last_Name'])
+
     # Merge weekly membership transaction data with reconciliationfiles to get cn_BioID
     for i in files:
         if i[-4:]=='.csv':
-            df= pd.read_csv(data_path+str(i),parse_dates=['CnBio_Birth_date'],skipinitialspace=True)
+            df= pd.read_csv(data_path+'\\'+str(i),parse_dates=['CnBio_Birth_date'],skipinitialspace=True)
             df.rename(columns={'Is Auto Renewal':'Is_Auto_Renewal','Payment Type':'Payment_Type'},inplace=True)
             df=df[(pd.isnull(df['CnBio_ID'])== True)&((df.Is_Auto_Renewal==True)|(df.Payment_Type=='RT'))]
             df = df.ix[:,['imod_member_id','First Name','Last Name','CnBio_Birth_date']]
@@ -57,7 +58,7 @@ def main():
             merge=pd.merge(df,recon_data,how='left',on=['First_Name','Last_Name','CnBio_Birth_date'])
             merge['review']='Merge'
             merge=merge.ix[:,['CnBio_ID','imod_member_id','review','First_Name', 'Last_Name','CnBio_Birth_date']]
-            merge.to_csv(export_path+'Merge_'+i,index=False)
+            merge.to_csv(export_path+'\\Merge_'+i,index=False)
             if len(merge.set_index(['First_Name','Last_Name']).index.get_duplicates()):
                 print("Duplicate records found, Please check \n[('First_Name', 'Last_Name')]")
                 print(merge.set_index(['First_Name','Last_Name']).index.get_duplicates())
