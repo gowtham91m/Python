e4ec47180b157c5debed2ae99a5d9f0d23ec65b4
GOWTHAM MALLIKARJUNA <gowtham.91m@gmail.com>
2017-02-28 12:24:24 -0600

    Update View.py

--- View.py
+++ View.py
@@ -1,33 +1,20 @@
+from pandastable import Table, TableModel
 from tkinter import *
-from pandastable import Table #, TableModel
 
 '''
-View Pandas dataframe in new window
-
-Usage
-From View import *
-View(df) # df is the name of your dataframe
+using:
+from pandastable import TableModel
+from View import view
+df = TableModel.getSampleData()
+view(df)
 '''
 
-class TestApp(Frame):
-    """Basic test frame for the table"""
-    def __init__(self,df, parent=None):
-        self.df=df
-        self.parent = parent
-        Frame.__init__(self)
-        self.main = self.master
-        self.main.geometry('600x400+200+100')
-        self.main.title('Table app')
-        f = Frame(self.main)
-        f.pack(fill=BOTH,expand=1)
-        #df = TableModel.getSampleData()
-        self.table = pt = Table(f, dataframe=self.df,
-                                showtoolbar=True, showstatusbar=True)
+class view():
+    def __init__(self,df):
+        self.tk=Tk()
+        self.tk.geometry('600x400+200+100')
+        self.tk.title('Table View')
+        self.f=Frame(self.tk)
+        self.f.pack(fill=BOTH,expand=1)
+        pt=Table(self.f,dataframe=df,showtoolbar=True, showstatusbar=True)
         pt.show()
-        return
-
-        
-def View(df):
-    app = TestApp(df)#launch the app
-    app.mainloop()
-
