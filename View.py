805b56c16ca6a12d534156f303664d55b40f78f4
GOWTHAM MALLIKARJUNA <gowtham.91m@gmail.com>
2017-03-04 09:09:05 -0600

    Update View.py

--- View.py
+++ View.py
@@ -1,20 +1,30 @@
-from pandastable import Table, TableModel
 from tkinter import *
+from pandastable import Table #, TableModel
 
 '''
-using:
-from pandastable import TableModel
+View Pandas dataframe in new window
+place this script in your python path directory
 from View import view
+Usage
+From View import *
 df = TableModel.getSampleData()
-view(df)
+View(df)
 '''
 
-class view():
-    def __init__(self,df):
-        self.tk=Tk()
-        self.tk.geometry('600x400+200+100')
-        self.tk.title('Table View')
-        self.f=Frame(self.tk)
-        self.f.pack(fill=BOTH,expand=1)
-        pt=Table(self.f,dataframe=df,showtoolbar=True, showstatusbar=True)
+class TestApp():
+    """Basic test frame for the table"""
+    def __init__(self,df, master=None):
+        self.df=df
+        self.main = master
+        self.main.attributes('-topmost',True)
+        self.main.geometry('600x400+200+100')
+        self.main.title('Table app')
+        f = Frame(self.main)
+        f.pack(fill=BOTH,expand=1)
+        pt = Table(f, dataframe=df, showstatusbar=True)
         pt.show()
+        return
+        
+def view(df):
+        root = Tk()
+        app = TestApp(df,root)
