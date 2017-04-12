5fa5588b8b973f1bbb4c28bf9e2d9c0ff7ef58a6
GOWTHAM MALLIKARJUNA <gowtham.91m@gmail.com>
2017-04-12 14:34:51 -0500

    Update View.py

--- View.py
+++ View.py
@@ -7,15 +7,11 @@ from pandastable import Table# , TableModel
 #df = TableModel.getSampleData()
 #view(df)
 
-class view():
-    def __init__(self,df,table_name=None):
-        self.df=df
-        self.main = Tk()
-        self.main.attributes('-topmost',True)
-        self.main.geometry('600x400+200+100')
-        self.main.title(table_name)
-        f = Frame(self.main)
-        f.pack(fill=BOTH,expand=1)
-        pt = Table(f, dataframe=df, showstatusbar=True)
-        pt.show()
-        return
+def view (df,table_name=None,topmost=True):
+    main = Tk()
+    main.attributes('-topmost',topmost)
+    main.title(table_name)
+    f = Frame(main)
+    f.pack(fill='both',expand=1)
+    pt = Table(f, dataframe=df, showstatusbar=True)
+    pt.show()
